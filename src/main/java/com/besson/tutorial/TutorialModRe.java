1e43871aa4f972461e85a74459b947ea20372b71
北山_Besson <137259636+BeiShanair@users.noreply.github.com>
2024-12-12 13:24:37 +0800

    Add Custom Item!

--- src/main/java/com/besson/tutorial/TutorialModRe.java
+++ src/main/java/com/besson/tutorial/TutorialModRe.java
@@ -1,5 +1,6 @@
 package com.besson.tutorial;
 
+import com.besson.tutorial.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
@@ -20,5 +21,7 @@ public class TutorialModRe implements ModInitializer {
 		// Proceed with mild caution.
 
 		LOGGER.info("Hello Fabric world!");
+		// 在主类的onInitialize方法中调用我们的初始化方法
+		ModItems.registerModItems();
 	}
 }
