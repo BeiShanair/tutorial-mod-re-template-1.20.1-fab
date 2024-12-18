51b2d788e1072077378a7b14dc8aeade679bcb4b
北山_Besson <137259636+BeiShanair@users.noreply.github.com>
2024-12-18 18:12:11 +0800

    Add Custom Item!

--- src/main/java/com/besson/tutorial/item/ModItems.java
+++ src/main/java/com/besson/tutorial/item/ModItems.java
@@ -16,6 +16,8 @@ public class ModItems {
     // 注册我们的第一个物品
     public static final Item ICE_ETHER = registerItem("ice_ether", new Item(new Item.Settings()));
 
+    public static final Item CARDBOARD = registerItems("materials/cardboard", new Item(new Item.Settings()));
+
     /*
        而采用原生的注册系统，我们不妨先去看看源代码
        以DIAMOND为例，我们可以看到其注册方法是这样的：（一共是三层，DIAMOND注册调用的是第一个方法）
