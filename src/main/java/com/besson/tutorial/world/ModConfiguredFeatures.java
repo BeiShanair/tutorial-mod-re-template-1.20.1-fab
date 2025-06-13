811fb747d251f56d8c5db58793d05d0e82f8fb35
北山_Besson <137259636+BeiShanair@users.noreply.github.com>
2025-06-13 17:03:07 +0800

    Ores Generation!

--- src/main/java/com/besson/tutorial/world/ModConfiguredFeatures.java
+++ src/main/java/com/besson/tutorial/world/ModConfiguredFeatures.java
@@ -2,20 +2,32 @@ package com.besson.tutorial.world;
 
 import com.besson.tutorial.TutorialModRe;
 import com.besson.tutorial.block.ModBlocks;
+import net.minecraft.block.Blocks;
 import net.minecraft.registry.Registerable;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
+import net.minecraft.registry.tag.BlockTags;
+import net.minecraft.structure.rule.BlockMatchRuleTest;
+import net.minecraft.structure.rule.RuleTest;
+import net.minecraft.structure.rule.TagMatchRuleTest;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.intprovider.ConstantIntProvider;
 import net.minecraft.world.gen.feature.*;
 import net.minecraft.world.gen.feature.size.TwoLayersFeatureSize;
 import net.minecraft.world.gen.foliage.BlobFoliagePlacer;
+import net.minecraft.world.gen.placementmodifier.*;
 import net.minecraft.world.gen.stateprovider.BlockStateProvider;
 import net.minecraft.world.gen.trunk.StraightTrunkPlacer;
+
+import java.util.List;
 
 public class ModConfiguredFeatures {
     public static final RegistryKey<ConfiguredFeature<?, ?>> ICE_ETHER_TREE_KEY = of("ice_ether_tree");
     public static final RegistryKey<ConfiguredFeature<?, ?>> SIMPLE_FLOWER_KEY = of("simple_flower");
+
+    public static final RegistryKey<ConfiguredFeature<?, ?>> ICE_ETHER_ORE_KEY = of("ice_ether_ore");
+    public static final RegistryKey<ConfiguredFeature<?, ?>> NETHER_ICE_ETHER_ORE_KEY = of("nether_ice_ether_ore");
+    public static final RegistryKey<ConfiguredFeature<?, ?>> END_ICE_ETHER_ORE_KEY = of("end_ice_ether_ore");
 
     public static RegistryKey<ConfiguredFeature<?, ?>> of(String id) {
         return RegistryKey.of(RegistryKeys.CONFIGURED_FEATURE, new Identifier(TutorialModRe.MOD_ID, id));
@@ -35,5 +47,25 @@ public class ModConfiguredFeatures {
                 new RandomPatchFeatureConfig(20, 4, 3,
                         PlacedFeatures.createEntry(Feature.SIMPLE_BLOCK,
                                 new SimpleBlockFeatureConfig(BlockStateProvider.of(ModBlocks.SIMPLE_FLOWER)))));
+
+        RuleTest stoneReplace = new TagMatchRuleTest(BlockTags.STONE_ORE_REPLACEABLES);
+        RuleTest deepSlateReplace = new TagMatchRuleTest(BlockTags.DEEPSLATE_ORE_REPLACEABLES);
+        RuleTest netherReplace = new TagMatchRuleTest(BlockTags.BASE_STONE_NETHER);
+        RuleTest endReplace = new BlockMatchRuleTest(Blocks.END_STONE);
+
+        List<OreFeatureConfig.Target> overWorldTargets = List.of(
+                OreFeatureConfig.createTarget(stoneReplace, ModBlocks.ICE_ETHER_ORE.getDefaultState()),
+                OreFeatureConfig.createTarget(deepSlateReplace, ModBlocks.ICE_ETHER_ORE.getDefaultState()));
+        List<OreFeatureConfig.Target> netherTargets = List.of(
+                OreFeatureConfig.createTarget(netherReplace, ModBlocks.ICE_ETHER_ORE.getDefaultState()));
+        List<OreFeatureConfig.Target> endTargets = List.of(
+                OreFeatureConfig.createTarget(endReplace, ModBlocks.ICE_ETHER_ORE.getDefaultState()));
+
+        ConfiguredFeatures.register(featureRegisterable, ICE_ETHER_ORE_KEY, Feature.ORE,
+                new OreFeatureConfig(overWorldTargets, 9));
+        ConfiguredFeatures.register(featureRegisterable, NETHER_ICE_ETHER_ORE_KEY, Feature.ORE,
+                new OreFeatureConfig(netherTargets, 12));
+        ConfiguredFeatures.register(featureRegisterable, END_ICE_ETHER_ORE_KEY, Feature.ORE,
+                new OreFeatureConfig(endTargets, 10));
     }
 }
--- src/main/java/com/besson/tutorial/world/ModPlacedFeatures.java
+++ src/main/java/com/besson/tutorial/world/ModPlacedFeatures.java
@@ -7,17 +7,30 @@ import net.minecraft.registry.RegistryEntryLookup;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
+import net.minecraft.world.gen.YOffset;
 import net.minecraft.world.gen.feature.*;
-import net.minecraft.world.gen.placementmodifier.BiomePlacementModifier;
-import net.minecraft.world.gen.placementmodifier.RarityFilterPlacementModifier;
-import net.minecraft.world.gen.placementmodifier.SquarePlacementModifier;
+import net.minecraft.world.gen.placementmodifier.*;
+
+import java.util.List;
 
 public class ModPlacedFeatures {
     public static final RegistryKey<PlacedFeature> ICE_ETHER_TREE_PLACED_KEY = ModPlacedFeatures.of("ice_ether_tree_placed");
     public static final RegistryKey<PlacedFeature> SIMPLE_FLOWER_PLACED_KEY = ModPlacedFeatures.of("simple_flower_placed");
 
+    public static final RegistryKey<PlacedFeature> ICE_ETHER_ORE_PLACED_KEY = ModPlacedFeatures.of("ice_ether_ore_placed");
+    public static final RegistryKey<PlacedFeature> NETHER_ICE_ETHER_ORE_PLACED_KEY = ModPlacedFeatures.of("nether_ice_ether_ore_placed");
+    public static final RegistryKey<PlacedFeature> END_ICE_ETHER_ORE_PLACED_KEY = ModPlacedFeatures.of("end_ice_ether_ore_placed");
+
     public static RegistryKey<PlacedFeature> of(String id) {
         return RegistryKey.of(RegistryKeys.PLACED_FEATURE, new Identifier(TutorialModRe.MOD_ID, id));
+    }
+
+    private static List<PlacementModifier> modifiers(PlacementModifier countModifier, PlacementModifier heightModifier) {
+        return List.of(countModifier, SquarePlacementModifier.of(), heightModifier, BiomePlacementModifier.of());
+    }
+
+    private static List<PlacementModifier> modifiersWithCount(int count, PlacementModifier heightModifier) {
+        return modifiers(CountPlacementModifier.of(count), heightModifier);
     }
 
     public static void bootstrap(Registerable<PlacedFeature> featureRegisterable) {
@@ -34,5 +47,15 @@ public class ModPlacedFeatures {
                 registryEntryLookup.getOrThrow(ModConfiguredFeatures.SIMPLE_FLOWER_KEY),
                 RarityFilterPlacementModifier.of(4), SquarePlacementModifier.of(),
                 PlacedFeatures.MOTION_BLOCKING_HEIGHTMAP, BiomePlacementModifier.of());
+
+        PlacedFeatures.register(featureRegisterable, ICE_ETHER_ORE_PLACED_KEY, registryEntryLookup.getOrThrow(ModConfiguredFeatures.ICE_ETHER_ORE_KEY),
+                modifiersWithCount(10,
+                        HeightRangePlacementModifier.uniform(YOffset.fixed(-80), YOffset.fixed(80))));
+        PlacedFeatures.register(featureRegisterable, NETHER_ICE_ETHER_ORE_PLACED_KEY, registryEntryLookup.getOrThrow(ModConfiguredFeatures.NETHER_ICE_ETHER_ORE_KEY),
+                modifiersWithCount(12,
+                        HeightRangePlacementModifier.uniform(YOffset.fixed(-80), YOffset.fixed(80))));
+        PlacedFeatures.register(featureRegisterable, END_ICE_ETHER_ORE_PLACED_KEY, registryEntryLookup.getOrThrow(ModConfiguredFeatures.END_ICE_ETHER_ORE_KEY),
+                modifiersWithCount(8,
+                        HeightRangePlacementModifier.uniform(YOffset.fixed(-80), YOffset.fixed(80))));
     }
 }
